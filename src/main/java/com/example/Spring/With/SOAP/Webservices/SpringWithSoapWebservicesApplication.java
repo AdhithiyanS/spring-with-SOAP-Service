875e27bc67236c9686ccd265c9c8801d34d07f5b
AdhithiyanS <adhithiyan11@gmail.com>
2021-09-23 22:51:40 +0530

    Service definition : request reponse with XSD made

---
+++ src/main/java/com/example/Spring/With/SOAP/Webservices/SpringWithSoapWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.With.SOAP.Webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithSoapWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithSoapWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/With/SOAP/Webservices/SpringWithSoapWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.With.SOAP.Webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithSoapWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
